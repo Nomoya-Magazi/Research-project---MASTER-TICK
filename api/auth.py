c9b84f7b76ee1d931c5249d3fb68a4808155fac2
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2023-03-07 11:12:43 +0200

    added extra info

--- api/auth.py
+++ api/auth.py
@@ -1,8 +1,13 @@
 import requests
+import logging
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+handler = logging.FileHandler('app.log')
+handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+app.logger.addHandler(handler)
 CORS(app, resources={r"/*": {"origins": "http://localhost:8080"}})
 
 JSON_SERVER_URL = "http://localhost:3000"
