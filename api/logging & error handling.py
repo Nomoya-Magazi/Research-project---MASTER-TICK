6a5724566bd3af891bb6cf42a2f4792c8dca1ec1
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2023-03-08 08:23:44 +0200

    still to be completed

--- api/logging & error handling.py
+++ api/logging & error handling.py
@@ -1,1 +1,11 @@
+import logging
 
+
+
+app.logger.setLevel(logging.INFO)
+handler = logging.FileHandler('app.log')
+handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+app.logger.addHandler(handler)
+
+
+code still needs to be completed
